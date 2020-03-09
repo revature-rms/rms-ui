f80fe11cb82ccaba4f91475f86e9b474d10bf572
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-03-09 14:38:05 -0400

    Partial functionality.

--- src/reducers/rooms-reducer.ts
+++ src/reducers/rooms-reducer.ts
@@ -2,14 +2,14 @@ import {IRoomsState} from ".";
 import {roomsTypes} from "../action-mappers/room-list-actions"
 
 const initialState:IRoomsState={
-    building:{},
+    building:{}
 }
 export const roomsReducer=(state=initialState,action:any)=>{
     switch(action.type) {
         case roomsTypes.GET_ALL_ROOMS: {
             return {
                 ...state,
-                building:action.payload.bldg
+                building:action.payload.building
             }
         }
         default: {
--- src/remote/room-list-search.ts
+++ src/remote/room-list-search.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const roomList = async() => {
     try {
-        let response=await(axios.get("https://api.myjson.com/bins/zlpki"))
+        let response=await(axios.get("https://api.myjson.com/bins/suof2"))
         return response.data;
     } catch (e) {
         console.log(e);
