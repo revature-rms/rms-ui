8432f02030473462f31f7f489d1b7b0eda24116a
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-03-16 12:04:01 -0400

    Fixed some routing issues.

--- src/utils/APIs/campusServiceApis.ts
+++ src/utils/APIs/campusServiceApis.ts
@@ -3,5 +3,6 @@ import Axios from 'axios';
 export const campusServiceApi =
     Axios.create({
         baseURL: "http://localhost:10000/search/campuses",
+        /* baseURL: "https://api.myjson.com/bins/95j3i" */
     })
    
