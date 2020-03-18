ee23d5bde771c6b0eb75b0ca0684d29097331a21
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-03-18 17:48:01 -0400

    updated all calls to new JSON objects.

--- src/remote/room-list-search.ts
+++ src/remote/room-list-search.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const roomList = async() => {
     try {
-        let response=await(axios.get("https://api.myjson.com/bins/suof2"))
+        let response=await(axios.get("https://api.myjson.com/bins/shzxq"))
         return response.data;
     } catch (e) {
         console.log(e);
