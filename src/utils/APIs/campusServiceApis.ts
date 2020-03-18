aa48aa03dd96ed917cc15614aedd9002dcec5c5a
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-03-18 12:05:47 -0400

    Added a commented-out connection to the currently nonfunctional search service.

--- src/utils/APIs/campusServiceApis.ts
+++ src/utils/APIs/campusServiceApis.ts
@@ -3,6 +3,6 @@ import Axios from 'axios';
 export const campusServiceApi =
     Axios.create({
         baseURL: "https://api.myjson.com/bins/95j3i",
-        /* baseURL: "https://api.myjson.com/bins/95j3i" */
+        /* baseURL: "http://localhost:8080/search/v1/campuses" */
     })
    
