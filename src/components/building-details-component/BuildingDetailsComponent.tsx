418aa69e4554609491fd37348b61276bdb1eb255
wandeltk <wandeltk@gmail.com>
2020-06-11 14:54:31 -0600

    added form elements and edittable state, still requires changes for editting to work, redux not in place

--- src/components/building-details-component/BuildingDetailsComponent.tsx
+++ src/components/building-details-component/BuildingDetailsComponent.tsx
@@ -1,142 +1,142 @@
-import React from 'react';
-import { allData } from '../../remote/allData';
-import { filterBuildingsFunction } from '../../utils/helper-functions/filterBuildings';
-import { sortBuildingFunction } from '../../utils/helper-functions/sortBuilidingFunction';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import Wrapper from '../../utils/div-wrapper/Wrapper';
+import { Grid, FormControl, InputLabel, Input, Card, Button } from '@material-ui/core';
+import { userInfo } from 'os';
+import { userState } from '../../reducers/login-reducer';
 
-export class BuildingDetailsComponent extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            campus: "",
-            searchTerm: "",
-            sortType: "ascending"
+export interface IBuildingDetailsProps {
+    thisBuilding: any
+}
+
+function BuildingDetailsComponent(){
+
+        const mockBuilding =  {id: 1, 
+                            name: "Muma College of Business",
+                            abbrName: "MCB",
+                            physicalAddress: "456 N Main st",
+                            trainingLead: "Bob",
+                            ameneties: [{ type: "coffee",
+                                        status: "low"
+                                        },
+                                        { type: "whiteboard",
+                                        status: "clean"
+                                        }],
+                            room: [{ roomNumber: "2035",
+                                    maxOccupancy: 30,
+                                    isActive: false
+                                    },
+                                    {roomNumber: "1005",
+                                    maxOccupancy: 15,
+                                    isActive: true
+                                    }],
+                            resourceMetadata: {resourceCreator: "John",
+                                                resourceCreationDateTime: "2020-03-03T13:22:31+00.00",
+                                                lastModifer: "Jeremy",
+                                                lastModifiedDateTime: "2020-03-05T10:21:35+00.00",
+                                                resourceOwner: "Abe"
+
+                                                }
         }
-    }
 
-    resources: any = "";
-    id: any
-    campus: any;
-    count = 0;
+        const [editting, setEdditing] = useState(false);
+        const [nameState, setName] = useState(mockBuilding.name);
+        const [abbrNameState, setAbbrName] = useState(mockBuilding.abbrName);
+        const [addressState, setAddress] = useState(mockBuilding.physicalAddress);
+        const [bManagerState, setBManager] = useState(mockBuilding.trainingLead);
+        const [building, setBuilding] = useState(mockBuilding)
+           
+        const enableEdit = () => {
+            setEdditing(true);
+        }   
 
-    async componentDidMount() {
-        //gets data from api
-        this.resources = await this.gatherData();
-        //checks the route parameter to populate the page with the right building
-        this.id = this.props.match.params.id;
-        this.setCampus();
-    }
-
-    setCampus = () => {
-        if (this.resources.campus) {
-            this.resources.campus.map((campus: any) => {
-                //looks for the campus in the campus object with the same name as the route parameter
-                if (campus["name"] === this.id) {
-                    if (this.state.campus.length === 0 || this.state.campus.name !== this.id)
-                    //will set that campus to the state if there is no campus set in state
-                        this.setState({
-                            campus: campus
-                        })
-                }
-            })
+        const save = () => {
+            setBuilding({...mockBuilding,
+                        name: nameState,
+                        abbrName: abbrNameState,
+                        physicalAddress: addressState,
+                        trainingLead: bManagerState
+                     })
         }
-    }
-    componentDidUpdate() {
-        //checks the route parameter to populate the page with the right building
-        this.id = this.props.match.params.id;
-        this.setCampus()
-    }
 
-    //makes a request to the api and gets all the data back
-    gatherData = async () => {
-        let apiData = await allData();
-        return apiData;
-    }
-
-    mapBuildings = () => {
-        if (this.state.searchTerm.length < 1) {
-            let buildings = this.state.campus.buildings.map((building: any) => building)
-            //returns sorted table row
-            return sortBuildingFunction(this.state.sortType, buildings).map((building: any) => this.makeTable(building))
+        const cancel = () => {
+            setEdditing(false);
         }
-        if (filterBuildingsFunction(this.state.campus, this.state.searchTerm).length === 0) {
-            return <h4>No Building Found!</h4>
+
+        const setInfo = (event: any) => {
+            switch(event.target.id){
+                case "name":
+                    setName(event.target.value);
+                case "abbrName":
+                    setAbbrName(event.target.value);
+                case "address":
+                    setAddress(event.target.value);
+                case "bManager":
+                    setBManager(event.target.value);
+            }
         }
-        //filters buildings
-        let filteredBuildings = filterBuildingsFunction(this.state.campus, this.state.searchTerm).map((building: any) => building);
-        //returns sorted table row
-        return sortBuildingFunction(this.state.sortType, filteredBuildings).map((building: any) => this.makeTable(building));
-    }
-
-    //changes search term state
-    onSearchChange = (e: any) => {
-        this.setState({
-            ...this.state,
-            searchTerm: e.target.value
-        })
-    }
-
-    //changes sort type state(sort type will be used in the sort function)
-    sortChanger = (e: any) => {
-        this.setState({
-            ...this.state,
-            sortType: e.target.value
-        })
-    }
-    //The table's sub-header
-    subHeader = () => {
-        return (
-            <>
-                {this.state.campus.abbrName}
-                &nbsp;
-                <input
-                    data-test="search"
-                    type="text"
-                    placeholder="Type building name or Lead trainer"
-                    onChange={this.onSearchChange}
-                />
-                &nbsp;
-                Sort:
-                <select
-                    onChange={this.sortChanger}
-                >
-                    <option value="ascending" selected>ascending</option>
-                    <option value="descending">descending</option>
-                </select>
-            </>
-        )
-    }
-    //takes a building and makes a table row
-    makeTable = (building: any) => {
-        return (
-            <tr key={`${building.trainingLead.firstName}${this.count++}`}>
-                <td><Link to={`/rooms/${building.name}`}><span className="colour-me">{building.name}</span></Link></td>
-                <td>{building.physicalAddress.unit_street}. {building.physicalAddress.city}, {building.physicalAddress.state}</td>
-                <td>{building.trainingLead.firstName} {building.trainingLead.lastName}</td>
-            </tr>
-        )
-    }
-
-    render() {
+        
+        
         return (
             <>
                 {/* Wrapper component is imported from utils/div-wrapper */}
-                <Wrapper data-test="main-content" title={this.state.campus ? this.state.campus.name : "Campus Name Here"} elements={this.state.campus ? this.subHeader()
-                    : "Campus abbreviation here."}>
-                    <div className="full-card">
-                        <div className="tblbox">
-                            <div className="tblhdr">Buildings in {this.state.campus ? this.state.campus.abbrName : "Building"}</div>
-                            <table>
-                                <tbody>
-                                    <tr><td><b>Building Name:</b></td><td><b>Address:</b></td><td><b>Training Lead:</b></td></tr>
-                                    {this.state.campus ? this.mapBuildings() : <tr><td>No data available</td><td>No data available</td><td>No data available</td><td>No data available</td></tr>}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                <Wrapper data-test="main-content" title={"Campus Name Here"} elements={"Campus abbreviation here."}>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Card className="full-card">
+                            <div id="building-form">
+                                <FormControl>
+                                    <InputLabel>Building Name: </InputLabel>
+                                    <Input id="name" value={building.name} disabled={!editting} onChange={setInfo} inputProps={{ 'aria-label': 'description' }} />
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel>Abbreviated Name: </InputLabel>
+                                    <Input id="abbrName" value={building.abbrName} disabled={!editting} inputProps={{ 'aria-label': 'description' }} />
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel>Address: </InputLabel>
+                                    <Input id="address" value={building.physicalAddress} disabled={!editting} inputProps={{ 'aria-label': 'description' }} />
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel>Building Manager: </InputLabel>
+                                    <Input id="bManager" value={building.trainingLead} disabled={!editting} inputProps={{ 'aria-label': 'description' }} />
+                                </FormControl>
+                            </div>
+                            <br/>
+                            {editting?
+                            <>
+                            <FormControl>
+                                <Button onClick={save}>Save</Button>
+                                <Button onClick={cancel}>Cancel</Button>
+                            </FormControl>
+                            </>
+                            :
+                            <FormControl>
+                                <Button onClick={enableEdit}>Edit</Button>
+                            </FormControl>}
+
+                            </Card>
+                        </Grid>
+
+                        <Grid item xs={8}>
+
+                        </Grid>
+
+                        <Grid item xs={4}>
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            
+                        </Grid>
+
+                    </Grid>
                 </Wrapper>
             </>
         )
-    }
 }
+
+export default BuildingDetailsComponent;
