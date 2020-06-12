409e438eafaa548d5113aa4248c7fbf8892d59f6
jeremy2975 <=Jeremy2975@yahoo.com>
2020-06-12 15:07:28 -0700

    refactored employee detail component. still working on fucntionality

--- src/components/employee-details-component/EmployeeDetailsComponent.tsx
+++ src/components/employee-details-component/EmployeeDetailsComponent.tsx
@@ -1,128 +1,130 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import Wrapper from '../../utils/div-wrapper/Wrapper';
 import EditEmpDetails from '../../utils/EditEmpDetailsModal';
 import { Link } from 'react-router-dom';
 import { updateEmployeeAPI } from '../../remote/employees';
+import { Employee } from '../../dtos/employee';
+import { Grid, FormControl, InputLabel, Input, Card, Button } from '@material-ui/core';
 
 
-interface IEmployeeDetailsProps {
+
+export interface IEmployeeDetailsProps {
     id: any,
     employees: any
 
 }
 
-export class EmployeeDetailsComponent extends React.Component<IEmployeeDetailsProps, any>{
+export function EmployeeDetailsComponent(){
 
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            employee: null,
-            fname: '',
-            lname: '',
-            email: '',
-            title: '',
-            department: ''
-        }
+    let mockEmployee = new Employee(
+        1,
+        "testFN",
+        "test LN",
+        "test@test.com",
+        "test title",
+        "test dept",
+        //@ts-ignore
+        null as ResourceMetadata
+    )
 
+    const [editing, setEditing] = useState(false);
+    const [firstNameState, setFirstName] = useState(mockEmployee.firstName);
+    const [lastNameState, setLastName] = useState(mockEmployee.lastName);
+    const [emailState, setEmail] = useState(mockEmployee.email);
+    const [titleState, setTitle] = useState(mockEmployee.title);
+    const [deptState, setDept] = useState(mockEmployee.department);
+    const [ResourceState, setResource] = useState(mockEmployee.resourceMetaData);
+    const [employee, setEmployee] = useState(mockEmployee);
+
+
+    const enableEdit = () =>{
+        setEditing(true);
     }
 
-    componentDidMount = () => {
-        if (this.props.employees != null) {
-            this.props.employees.Employee.filter((employee: any) => {
-                return employee.id === this.props.id
-            }).map((employee: any) => {
-                this.setState({
-                    ...this.state,
-                    employee: employee,
-                    fname: employee.firstName,
-                    lname: employee.lastName,
-                    email: employee.email,
-                    title: employee.title,
-                    department: employee.department,
-                    updateInfo: ''
-                })
-            })
-        }
+    const save = ()=>{
+        setEmployee({...mockEmployee,
+                    firstName: firstNameState,
+                    lastName: lastNameState,
+                    email: emailState,
+                    title: titleState,
+                    department: deptState
+        });
+        setEditing(false);
     }
 
-    handleChange = (event:any) => {
-        const {name, value} = event.target;
-        this.setState({
-            ...this.state,
-            [name]: value
-        })
+    const cancel = ()=>{
+        setEditing(false);
     }
 
-    handleUpdate = () => {
-
-        // updated employee json
-        let updatedEmployee = {
-            firstName: this.state.fname,
-            lastName: this.state.lname,
-            email: this.state.email,
-            title: this.state.title,
-            department: this.state.department
-        }
-
-        let empId = this.props.id
-
-        // Calling the API to update employee data
-        updateEmployeeAPI(empId, updatedEmployee).then((res:any) => {
-            this.setState({updateInfo: 'Employee successfully updated!'});
-            console.log(res);   
-        }).catch(err => {
-            this.setState({updateInfo:'Failed to update employee. Try again'})
-            console.log(err);
+    const setInfo = (event: any) =>{
+        switch(event.target.id){
+            case "firstName":
+                setFirstName(event.target.value);
+                break;
+            case"lastName":
+                setLastName(event.target.value);
+                break;
+            case "email":
+                setEmail(event.target.value);
+                break;  
+            case "title":
+                setTitle(event.target.value);
+                break;
+            case "department":
+                setDept(event.target.value);
+                break;
             
-        })
-        
-    }
-
-    render() {
-        if (this.state.employee != null) {
-            const employee = this.state.employee;
-
-            return (
-                // content wrapped in imported wrapper function in order to enforce same styling logic for all components.
-                <Wrapper data-test="main-content" title={employee.firstName + " " + employee.lastName} elements={<Link to="/employees">&lt; Back</Link>}>
-                    <div className="full-card">
-                        <h4>Employee Details</h4>
-                        <b>ID:</b> {employee.id}<br />
-                        <b>First Name:</b>  {employee.firstName}<br />
-                        <b>Last Name:</b>  {employee.lastName}<br />
-                        <b>Email:</b>  {employee.email}<br />
-                        <b>Title:</b> {employee.title}<br />
-                        <b>Department:</b>  {employee.department}
-
-                        <br/>
-                        <EditEmpDetails  // this imports a modal that receive props needed to update the details of an employee selected.
-                        fname = {this.state.fname}
-                        lname = {this.state.lname}
-                        email = {this.state.email}
-                        title = {this.state.title}
-                        department = {this.state.department}
-                        handleChange= {this.handleChange}
-                        updateEmpDetails = {this.handleUpdate}
-                        updateMessage = {this.state.updateInfo}                       
-                        />
-                    </div>
-                    <div className="half-card">
-                        <b>Resource Created By:</b> {employee.resourceMetadata.resourceCreator.username}<br />
-                        <b>Creation Date:</b> {employee.resourceMetadata.resourceCreationDateTime.substring(0,10)}<br />
-                        <b>Last Modified:</b> {employee.resourceMetadata.lastModifiedDateTime.substring(0,10)}<br />
-                        <b>Modified By:</b> {employee.resourceMetadata.lastModifier.username}<br />
-                        <b>Resource Owner:</b> {employee.resourceMetadata.resourceOwner.username}<br />
-
-                    </div>
-                </Wrapper>
-            );
-        } else {
-            return ( // if no id selected, nothing is returned.
-                <Wrapper>
-                    <h3>Select an employee to view the details. <Link to="/employees">Go back to employees table</Link> </h3>
-                </Wrapper>
-            )
         }
     }
+
+    return (
+        <>
+            <Wrapper data-test="main-content" title={employee.firstName} elements = {"Campus Name Here"}>
+                <Grid container> 
+                    <Grid item xs={12}>
+                        <Card className = "full-card">
+                            <div id = "employee-form">
+                                <FormControl>
+                                    <InputLabel>Employee First Name:</InputLabel>
+                                    {editing?
+                                    <Input id = "firstName" defaultValue={employee.firstName} disabled={!editing} onChange={setInfo} inputProps={{'aria-label':'description'}} />:
+                                    <Input id = "firstName" value={employee.firstName} disabled={!editing} onChange={setInfo} inputProps={{'aria-label':'description'}} />}
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel> Employee Last Name:</InputLabel>
+                                    {editing?
+                                    <Input id = "lastName" defaultValue={employee.lastName} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />:
+                                    <Input id = "lastName" value={employee.lastName} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />}
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel> Employee Email:</InputLabel>
+                                    {editing?
+                                    <Input id = "email" defaultValue={employee.email} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />:
+                                    <Input id = "email" value={employee.email} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />}
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel>Employee Title:</InputLabel>
+                                    {editing?
+                                    <Input id = "title" defaultValue={employee.title} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />:
+                                    <Input id = "title" value={employee.title} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />}
+                                </FormControl>
+                                <br/>
+                                <FormControl>
+                                    <InputLabel>Employee Department</InputLabel>
+                                    {editing?
+                                    <Input id = "department" defaultValue={employee.department} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />:
+                                    <Input id = "department" value={employee.department} disabled={!editing} onChange = {setInfo} inputProps={{'aria-label':'description'}} />}
+                                </FormControl>
+                            </div>
+
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Wrapper>
+        </>
+    )
 
 }
