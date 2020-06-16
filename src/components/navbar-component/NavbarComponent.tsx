1f635cc065bc5c7451cf9980b7bbc39ba820a4c9
jbostik <63011664+jbostik@users.noreply.github.com>
2020-06-16 12:13:39 -0400

    resolving merge conflicts

--- src/components/navbar-component/NavbarComponent.tsx
+++ src/components/navbar-component/NavbarComponent.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { SearchComponent } from '../search-component/SearchComponent';
 import { logout } from '../../utils/LogoutFunction';
 
-import "../../styles/campus.scss";
+import "../../styles/navbar.scss";
 
 export default function NavbarComponent() {
 
