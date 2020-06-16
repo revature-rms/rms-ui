65b66e28b27fee3fb0f4cc0f44a8e91ce3475877
jbostik <63011664+jbostik@users.noreply.github.com>
2020-06-16 13:01:22 -0400

    added back src/remote/room-details-search temporarily

---
+++ src/remote/room-details-search.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const testRoom = async () => {
+    try {
+        let response = await (axios.get("https://api.myjson.com/bins/1bfd9a"))
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        return {
+            return: null
+        }
+    }
+}
