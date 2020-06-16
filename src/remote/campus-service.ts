a8733b911b8c72713eeb9e10859bcde34060c120
jbostik <63011664+jbostik@users.noreply.github.com>
2020-06-16 13:00:44 -0400

    changed the casing of getAllCampusApi

--- src/remote/campus-service.ts
+++ src/remote/campus-service.ts
@@ -1,21 +1,21 @@
 import { apiClient } from '.';
 
 /**Function to get all campus from campus microservice.*/
-export const getAllcampusAPI = async () => {
+export const getAllCampusAPI = async () => {
     return await apiClient.get(`campus/v2/campus`);
 }
 
 /**Function to get campus by id*/
-export const getcampusByIdAPI = (id:number) => {
+export const getCampusByIdAPI = (id:number) => {
     return apiClient.get(`campus/v2/campus/${id}`);
 }
 
 /**Function to create an campus*/
-export const createcampusAPI = (data: any) => {
+export const createCampusAPI = (data: any) => {
     return apiClient.post('campus/v2/campus', data);
 }
 
 /**Function to update an existing campus*/
-export const updatecampusAPI = (id:number, data: any) => {
+export const updateCampusAPI = (id:number, data: any) => {
     return apiClient.put(`campus/v2/campus/${id}`, data);
 }
