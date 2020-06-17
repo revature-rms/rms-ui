1c0937a62620c5a0a759413fd67ea89338e03201
donxiya <31514669+donxiya@users.noreply.github.com>
2020-06-17 09:43:48 -0500

    Update HomeComponent.tsx

--- src/components/Home-component/HomeComponent.tsx
+++ src/components/Home-component/HomeComponent.tsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Wrapper from '../../utils/div-wrapper/Wrapper';
 import { Link } from "react-router-dom";
 import MaterialTable from 'material-table';
-import {getAllcampusAPI} from '../../remote/campus'
+import { getAllcampusAPI } from '../../remote/campus'
 import { AppUser } from '../../dtos/appUser';
 import { prependOnceListener } from 'process';
 import { roomList } from '../../remote/room-list-search';
+import Card from '@material-ui/core/Card';
 
 export interface IHomeProps {
     authUser: AppUser;
@@ -32,7 +33,7 @@ export function HomeComponent(props: IHomeProps) {
         if (props.authUser.role == "Trainer") getAssociates();
         if (props.authUser.role == "Building Manager") getRooms();
     }, []);
-    
+
     const getBuildings = async () => {
         // await getCampuses();
         let campuses;
@@ -52,10 +53,10 @@ export function HomeComponent(props: IHomeProps) {
 
     const getAssociates = () => {
         let associates = [
-                {name: 'a'},
-                {name: 'b'},
-                {name: 'c'}
-            ]
+            { name: 'a' },
+            { name: 'b' },
+            { name: 'c' }
+        ]
         console.log(associates)
         //@ts-ignore
         setAssociates(associates);
@@ -64,56 +65,54 @@ export function HomeComponent(props: IHomeProps) {
     //Home page rendering for an admin user.
     return (
         <>
-            {props.authUser?.role == 'Admin' ? 
-            <Wrapper data-test="main-content" title={"Current User"} elements={"Campus abbreviation here."}>
-                <div className="full-card">
-                    <div className="tblbox">
+            {props.authUser?.role == 'Admin' ?
+                <Card>
+                    <div className="table-wrapper">
                         < MaterialTable
-                            columns = {[
-                                { title: 'Campus Name', field: 'name'},
+                            columns={[
+                                { title: 'Campus Name', field: 'name' },
                                 { title: 'Training Manager', field: 'trainingManagerId' },
-                                { title: 'Number of Buildings', field: 'buildings.length'}
+                                { title: 'Number of Buildings', field: 'buildings.length' }
                             ]}
-                            data = {campus}
-                            title = "Campus"
-
+                            data={campus}
+                            title="Campus"
                         />
                     </div>
-                </div>
-            </Wrapper>
-            : props.authUser?.role == 'Trainer' ? 
-            <Wrapper data-test="main-content" title={"Batch Name"} elements={"Room Number"}>
-                <div className="full-card">
-                    <div className="tblbox">
-                        < MaterialTable
-                            columns = {[
-                                { title: 'Associate Name', field: 'name'}
-                            ]}
-                            data = {associates}
-                            title = "Associates"
+                    <br />
+                </Card>
+                : props.authUser?.role == 'Trainer' ?
+                    <Wrapper data-test="main-content" title={"Batch Name"} elements={"Room Number"}>
+                        <div className="full-card">
+                            <div className="tblbox">
+                                < MaterialTable
+                                    columns={[
+                                        { title: 'Associate Name', field: 'name' }
+                                    ]}
+                                    data={associates}
+                                    title="Associates"
 
-                        />
-                    </div>
-                </div>
-            </Wrapper>
-            : props.authUser?.role == 'Building Manager' ? 
-            <Wrapper data-test="main-content" title={"Building Name"} elements={"Building ID"}>
-                <div className="full-card">
-                    <div className="tblbox">
-                        < MaterialTable
-                            columns = {[
-                                { title: 'Room Number', field: 'roomNumber' },
-                                { title: 'Room Occupancy', field: 'maxOccupancy' }
-                            ]}
-                            data = {rooms}
-                            title = "Rooms"
+                                />
+                            </div>
+                        </div>
+                    </Wrapper>
+                    : props.authUser?.role == 'Building Manager' ?
+                        <Wrapper data-test="main-content" title={"Building Name"} elements={"Building ID"}>
+                            <div className="full-card">
+                                <div className="tblbox">
+                                    < MaterialTable
+                                        columns={[
+                                            { title: 'Room Number', field: 'roomNumber' },
+                                            { title: 'Room Occupancy', field: 'maxOccupancy' }
+                                        ]}
+                                        data={rooms}
+                                        title="Rooms"
 
-                        />
-                    </div>
-                </div>
-            </Wrapper>
-            : props.authUser?.role == 'Training Site Manager' ? <></> 
-            : <></>
+                                    />
+                                </div>
+                            </div>
+                        </Wrapper>
+                        : props.authUser?.role == 'Training Site Manager' ? <></>
+                            : <></>
             }
         </>
     )
