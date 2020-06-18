1e22dea781a71bc5f797d092cf61975e748b8d8b
wandeltk <wandeltk@gmail.com>
2020-06-18 13:09:27 -0600

    added setThisBuilding to DispatchToProps

--- src/components/campus-details-component/CampusDetailsContainer.tsx
+++ src/components/campus-details-component/CampusDetailsContainer.tsx
@@ -2,22 +2,16 @@ import BuildingListComponent from "./CampusDetailsComponent";
 import { connect } from "react-redux";
 import { IState } from "../../reducers";
 import {getAllCampuses} from "../../action-mappers/campus-action";
-import {updateId} from "../../action-mappers/campus-action"
+import {setThisBuilding} from "../../action-mappers/building-action"
 
 const mapStateToProps = (state:IState) => {
     return {
-        //states needed for the Campus component
-        campuses: state.campusState.campuses,
-        campusMessage: state.campusState.campusMessage,
-        id: state.campusState.id
     }
 }
 
 
 const mapDispatchToProps = {
-    //getting all Campuses from Campus action mapper
-    getAllCampuses,
-    updateId
+    setThisBuilding
 }
 
 // connecting the Campus component with redux info
