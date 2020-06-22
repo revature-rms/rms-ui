1e1f25cb382bcd27a673c6f3f222859646447f0e
jbostik <63011664+jbostik@users.noreply.github.com>
2020-06-22 13:00:01 -0400

    Added npm run coverage, re-formatted some components to be easier to test, and added tests for the home, login, and navbar components.

---
+++ src/components/login-component/test/LoginComponent.spec.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import LoginComponent, { ILoginProps } from '../LoginComponent'
+import { shallow, configure, mount } from 'enzyme';
+import { FindByTestAttr } from '../../../utils/helper-functions/testUtils';
+import * as mockRemote from '../../../remote/auth-service';
+import Adapter from 'enzyme-adapter-react-16';
+import { FormControl } from '@material-ui/core';
+import { AppUser } from '../../../dtos/appUser';
+
+
+configure({adapter: new Adapter()});
+
+const setState = jest.fn();
+const useStateMock: any = (initState: any) => [initState, setState]
+
+jest.mock('../../../remote/auth-service', () => {
+
+    return {
+        userLogin: jest.fn()
+    };
+});
+
+beforeEach(() => {
+    //Not actually setting the authUser to undefined, need to figure this out
+    (props.login as jest.Mock).mockClear();
+    (mockRemote.userLogin as jest.Mock).mockClear();
+})
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+const props: ILoginProps = {
+    authUser: undefined,
+    loginMessage: "",
+    login: jest.fn()
+};
+
+const loginComponent = <LoginComponent {...props} />
+
+describe('LoginComponent', () => {
+
+    const setState = jest.fn();
+    const useStateMock: any = (init: any) => [init, setState];
+
+    // const setup = (props:any = {login: () => {return "test"}}, state = 0) => {
+    //     const wrapper:any = shallow(<LoginComponent {...props} />)
+    //     if (state) wrapper.setState(state);
+    //     return wrapper;
+    // }
+
+    // test('renders without error', () => {
+    //     const wrapper = setup();
+    //     //find element with the data-test value
+    //     const appComponent = FindByTestAttr(wrapper, 'main-content');
+    //     expect(appComponent.length).toBe(1);
+    // });
+
+    // test('component did update',  () => {
+    //     const wrapper:any = setup();
+    //     wrapper.instance().signUserIn()
+    // });
+
+    it('Should render', () => {
+        const wrapper = shallow(loginComponent);
+        expect(wrapper.exists()).toBeTruthy();
+    })
+
+    it('Should render 2 inputs', () => {
+        const wrapper = mount(loginComponent);
+        expect(wrapper.find("input")).toHaveLength(2)
+    })
+
+    it('Typing into input.username trigger state hook on username', () => {
+        let wrapper = mount(loginComponent);
+        console.log(wrapper.find('input#username'))
+        wrapper.find('input#username').simulate('change', {
+            target: { 
+                id: 'username',
+                value: 'foo' 
+            }
+        });
+        expect(wrapper.find('input#username')).toHaveLength(1);
+        expect(wrapper.find('input#username').prop('value')).toEqual('foo');
+    });
+
+    it('Typing into input.password trigger state hook on password', () => {
+        let wrapper = mount(loginComponent);
+        console.log(wrapper.find('input#password'))
+        wrapper.find('input#password').simulate('change', {
+            target: { 
+                id: 'password',
+                value: 'top-secret' 
+            }
+        });
+        expect(wrapper.find('input#password')).toHaveLength(1);
+        expect(wrapper.find('input#password').prop('value')).toEqual('top-secret');
+    });
+
+    it('Typing into input.username trigger state hook on username', () => {
+        let wrapper = mount(loginComponent);
+        console.log(wrapper.find('input#username'))
+        wrapper.find('input#username').simulate('change', {
+            target: { 
+                id: 'username',
+                value: 'foo' 
+            }
+        });
+        wrapper.find('input#password').simulate('change', {
+            target: { 
+                id: 'password',
+                value: 'top-secret' 
+            }
+        });
+        wrapper.find('button#loginButton').simulate('click', {})
+        expect(wrapper.find('input#username').prop('value')).toEqual('foo');
+        expect(wrapper.find('input#password').prop('value')).toEqual('top-secret');
+        expect(props.login).toHaveBeenCalled();
+    });
+
+})
--- src/components/navbar-component/test/NavbarComponent.test.tsx
+++ src/components/navbar-component/test/NavbarComponent.test.tsx
@@ -1,24 +1,51 @@
-import NavbarComponent from '../NavbarComponent'
-import { shallow } from 'enzyme';
 import React from 'react';
+import NavbarComponent, { INavbarProps } from '../NavbarComponent'
+import { shallow, configure, mount } from 'enzyme';
 import { FindByTestAttr } from '../../../utils/helper-functions/testUtils';
+import * as mockRemote from '../../../remote/campus-service';
+import Adapter from 'enzyme-adapter-react-16';
+import { FormControl, Grid } from '@material-ui/core';
+import { AppUser } from '../../../dtos/appUser';
+import { BrowserRouter } from 'react-router-dom';
 
-const setup = (props = {}, state = 0) => {
-    const wrapper = shallow(<NavbarComponent {...props} />)
-    if (state) wrapper.setState(state);
-    return wrapper;
-}
 
-test('renders top bar without error', () => {
-    const wrapper = setup();
-    //find element with the data-test value
-    const appComponent = FindByTestAttr(wrapper, 'navbar-top');
-    expect(appComponent.length).toBe(1);
-});
+configure({adapter: new Adapter()});
 
-test('renders side navbar without error', () => {
-    const wrapper = setup();
-    //find element with the data-test value
-    const appComponent = FindByTestAttr(wrapper, 'navbar-side');
-    expect(appComponent.length).toBe(1);
-});
+const setState = jest.fn();
+const useStateMock: any = (initState: any) => [initState, setState]
+
+
+const props: INavbarProps = {
+    currentUser: new AppUser(1, "username", "password", 1, ['Admin', 'Trainer', 'Building Manager', 'Training Site Manager'])
+};
+
+const navbarComponent = <NavbarComponent {...props} />
+
+describe('NavbarComponent', () => {
+
+    const setState = jest.fn();
+    const useStateMock: any = (init: any) => [init, setState];
+
+    it('Should render', () => {
+        const wrapper = shallow(navbarComponent);
+        expect(wrapper.exists()).toBeTruthy();
+    })
+
+    it('Should render 9 divs', () => {
+        const wrapper = mount(<BrowserRouter>{navbarComponent}</BrowserRouter>);
+        expect(wrapper.find("div")).toHaveLength(9);
+    })
+
+    it('Should render 6 divs', () => {
+        //This is pretty bad, maybe find another way to change roles in the future?
+        const testProps: INavbarProps = {
+            currentUser: new AppUser(1, "username", "password", 1, [])
+        };
+        const testNavbarComponent = <NavbarComponent {...testProps}/>
+        const wrapper = mount(<BrowserRouter>{testNavbarComponent}</BrowserRouter>);
+        expect(wrapper.find("div")).toHaveLength(6);
+    })
+
+})
+
+
