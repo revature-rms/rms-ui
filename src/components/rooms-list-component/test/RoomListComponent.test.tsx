f8c8ee3bbed3848bc189ba3c36f7023bf581ef2c
wandeltk <wandeltk@gmail.com>
2020-06-23 10:08:30 -0600

    init testing for Room list

--- src/components/rooms-list-component/test/RoomListComponent.test.tsx
+++ src/components/rooms-list-component/test/RoomListComponent.test.tsx
@@ -1,64 +1,13 @@
-import { RoomListComponent } from '../RoomListComponent'
+import RoomListComponent from '../RoomListComponent'
 import { shallow } from 'enzyme';
 import React from 'react';
 import { FindByTestAttr } from '../../../utils/helper-functions/testUtils';
 
-let propsData = {
-    getAllProps: () => { },
-    getAllRooms: () => { },
-    building: {
-        resourceMetadata: {
-            resourceCreationDateTime: '',
-            lastModifiedDateTime: '',
-            lastModifier: {
-                username: ''
-            },
-            resourceCreator: {
-                username: ''
-            },
-            resourceOwner: {
-                username: ''
-            }
-        }
+jest.mock('mocking get functions for campus buildings, and rooms', () => {
+    return {
+       getCampusByOwnerId: jest.fn(),
+       getBuildingByOwnerId: jest.fn(),
+       getRoomByOwnerId: jest.fn(),
+        
     }
-}
-const setup = (props: any = propsData, state = 0) => {
-    const wrapper = shallow(<RoomListComponent {...props} />)
-    if (state) wrapper.setState(state);
-    return wrapper;
-}
-
-test('renders room without error', () => {
-    const wrapper = setup();
-    //find element with the data-test value
-    const appComponent = FindByTestAttr(wrapper, 'main-content');
-    expect(appComponent.length).toBe(1);
-});
-
-test('map rooms', () => {
-    const wrapper:any = setup();
-    const FakeFun = jest.spyOn(wrapper.instance(), 'mapRooms');
-    wrapper.instance().render();
-    expect(FakeFun).toHaveBeenCalled();
-});
-
-test('map rooms', () => {
-    const wrapper:any = setup();
-    const FakeFun = jest.spyOn(wrapper.instance(), 'makeTable');
-    wrapper.instance().render();
-    expect(FakeFun).toHaveBeenCalled();
-});
-
-test('map amenities', () => {
-    const wrapper:any = setup();
-    const FakeFun = jest.spyOn(wrapper.instance(), 'mapAmenities');
-    wrapper.instance().render();
-    expect(FakeFun).toHaveBeenCalled();
-});
-
-test('Make lists', () => {
-    const wrapper:any = setup();
-    const FakeFun = jest.spyOn(wrapper.instance(), 'makeList');
-    wrapper.instance().render();
-    expect(FakeFun).toHaveBeenCalled();
-});
+})
